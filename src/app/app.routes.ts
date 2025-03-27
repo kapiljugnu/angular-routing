ee9893c3623a73eae39def32c009f5e34c88dfd6
Kapil Jugnu <kr_kapil@hotmail.com>
2025-03-27 16:00:04 +0530

    angular guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent, resolveUserName } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes }from './users/users.routers';
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+} 
  
 export const routes: Routes = [
     {
@@ -18,6 +28,7 @@ export const routes: Routes = [
         path: 'users/:userId', // userId dynamic path segment
         component: UserTasksComponent,
         children: userRoutes,
+        canMatch: [dummyCanMatch],
         data: {
             message: "Hello"
         },
